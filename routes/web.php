83c1d91f963967e53ec60283be7fca94b598cbbd
jiya yadav <jiyayadav102@gmail.com>
2023-12-21 22:00:52 +0545

    Blade files and Controllers for register page

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\front\ContactController;
 use App\Http\Controllers\front\HomeController;
 use App\Http\Controllers\front\MenupriceController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 
 /*
@@ -30,15 +28,4 @@ Route::get('/about',[AboutController::class,'index'])->name('home.about');
 Route::get('/menu-pricing',[MenupriceController::class,'index'])->name('home.menu');
 Route::get('/contact',[ContactController::class,'index'])->name('home.contact');
 
-Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
-
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
-});
-
-// require __DIR__.'/authorization.php';
+require __DIR__.'/authorization.php';
